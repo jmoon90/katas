897a28fd9afebeca9aa5cf9b6c19cd447862b3bf
John Moon <john.moon@livingsocial.com>
2016-03-07 10:56:18 -0500

    Refactored location method   - case statement into hash_library   - recognize edge case of no sub_table data in a new method

--- refactor_metric_fu/metric_fu.rb
+++ refactor_metric_fu/metric_fu.rb
@@ -1,20 +1,16 @@
 def location(item, value)
-
   sub_table = get_sub_table(item, value)
-  if(sub_table.length==0)
-    raise MetricFu::AnalysisError, "the #{item.to_s} '#{value.to_s}' ￼ ￼"\ "does not have any rows in the analysis table"
-  else
-    first_row = sub_table[0]
-    case item
-    when :class
-      MetricFu::Location.get( first_row.file_path, first_row.class_name, nil)
-    when :method
-      MetricFu::Location.get(first_row.file_path, first_row.class_name, first_row.method_name) when :file
-    when :file
-      MetricFu::Location.get( first_row.file_path, nil, nil)
-    else
-      raise ArgumentError, "Item must be :class, :method, or :file"
-    end
+  assert_sub_table_has_value(item, value, sub_table)
+  first_row = sub_table[0]
+
+  metric_location_library = { :class => MetricFu::Location.get(first_row.file_path, first_row.class_name, nil),
+                              :method => MetricFu::Location.get(first_row.file_path, first_row.class_name, first_row.method_name),
+                              :file => MetricFu::Location.get( first_row.file_path, nil, nil) }
+  metric_location_library.fetch(item) do
+    raise ArgumentError, "Item must be :class, :method, or :file"
   end
 end
 
+def assert_sub_table_has_value(item, value, sub_table)
+  raise MetricFu::AnalysisError, "the #{item.to_s} '#{value.to_s}'￼"\ "does not have any rows in the analysis table" if(sub_table.length==0)
+end
