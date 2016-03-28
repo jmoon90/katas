999de1dcacfd741614b4cc2958f62b9a6ae40bb3
John Moon <john.moon@livingsocial.com>
2016-03-28 14:47:50 -0400

    Refactor and create FreeTime Class

--- freetime/freetime.rb
+++ freetime/freetime.rb
@@ -3,61 +3,111 @@ require 'byebug'
 START_TIME = 9
 MEETING_BLOCK = 0.5
 
-# def human_readable
-# end
+class FreeTime
+  def initialize(meeting_times)
+    @meeting_times = meeting_times
+    @final_meeting_schedule = []
+    @skip_indexes = []
+  end
 
-def condense_meeting_times(meeting_times)
-  final_meeting_schedule = []
+  # FreeTime.new([ [0, 1], [4, 8], [3, 5], [10, 12], [9, 10] ]).condense_meeting_times
+  def condense_meeting_times
+    meeting_times.each_with_index do |meeting_time, outter_index|
+      next if @skip_indexes.include?(outter_index)
+      @outer_meeting_time = meeting_time
+      @best_condensed_start_time = nil
+      @best_condensed_end_time = nil
 
-  meeting_times.each_with_index do |meeting_time, outter_index|
-    @start_time = meeting_time[0]
-    @end_time = meeting_time[1]
-
-    included = false
-    meeting_times.each_with_index do |inner_meeting_time, inner_index|
-      next if inner_index == outter_index
-
-      inner_start_time = inner_meeting_time[0]
-      inner_end_time = inner_meeting_time[1]
-      if inner_start_time.between?(start_time, end_time)
-        s_times = [start_time, inner_start_time].sort[0]
-        e_times = [end_time, inner_end_time].sort[-1]
-
-        included = true
-        meeting_times.delete_at(inner_index)
-        final_meeting_schedule << [s_times, e_times]
-      elsif inner_meeting_time.include?(start_time)
-        s_times = [start_time, inner_start_time].sort[0]
-        e_times = [end_time, inner_end_time].sort[-1]
-
-        included = true
-        meeting_times.delete_at(inner_index)
-        final_meeting_schedule << [s_times, e_times]
-      elsif inner_meeting_time.include?(end_time)
-        s_times = [start_time, inner_start_time].sort[0]
-        e_times = [end_time, inner_end_time].sort[-1]
-
-        included = true
-        meeting_times.delete_at(inner_index)
-        final_meeting_schedule << [s_times, e_times]
+      included = false
+      meeting_times.each_with_index do |inner_meeting_time, inner_index|
+        next if meeting_time == inner_meeting_time
+        @inner_meeting_time = inner_meeting_time
+        if meets_any_time_slot_condition?
+          included = true
+          if @best_condensed_start_time == nil || @best_condensed_start_time > condensed_start_time
+            @best_condensed_start_time = condensed_start_time
+          end
+          if @best_condensed_end_time == nil || @best_condensed_end_time < condensed_end_time
+            @best_condensed_end_time = condensed_end_time
+          end
+          byebug if outter_index == 2
+          @skip_indexes << inner_index
+        end
+      end
+      if included == false
+        final_meeting_schedule << meeting_time
+      else
+        final_meeting_schedule << [@best_condensed_start_time, @best_condensed_end_time]
       end
     end
-
-    if included == false
-      final_meeting_schedule << meeting_time
-    end
   end
-  puts final_meeting_schedule
+
+  def condensed_meeting_schedule
+    condense_meeting_times
+    puts final_meeting_schedule
+  end
+
+  private
+
+  attr_accessor :final_meeting_schedule, :inner_meeting_time, :outer_meeting_time, :meeting_times
+
+  def condensed_start_time
+    [start_time, inner_start_time].sort[0]
+  end
+
+  def condensed_end_time
+    [end_time, inner_end_time].sort[-1]
+  end
+
+  def meets_any_time_slot_condition?
+    inner_meeting_time.include?(end_time) ||
+      inner_meeting_time.include?(start_time) ||
+      inner_start_time.between?(start_time, end_time) ||
+      inner_end_time.between?(start_time, end_time) ||
+      next_meeting_block? ||
+      between_best_condensed_time? ||
+      next_best_condensed_time?
+  end
+
+  def start_time
+    @start_time = outer_meeting_time[0]
+  end
+
+  def end_time
+    @end_time = outer_meeting_time[1]
+  end
+
+  def next_meeting_block?
+    outer_meeting_time.include?(@inner_start_time + 1) ||
+      outer_meeting_time.include?(@inner_start_time - 1)
+  end
+
+  def between_best_condensed_time?
+    return unless @best_condensed_start_time
+    return unless @best_condensed_end_time
+    inner_start_time.between?(@best_condensed_start_time, @best_condensed_end_time) ||
+      inner_end_time.between?(@best_condensed_start_time, @best_condensed_end_time)
+  end
+
+  def next_best_condensed_time?
+    return unless @best_condensed_start_time
+    return unless @best_condensed_end_time
+    inner_meeting_time.include?(@best_condensed_start_time + 1) ||
+      inner_meeting_time.include?(@best_condensed_start_time - 1) ||
+      inner_meeting_time.include?(@best_condensed_end_time + 1) ||
+      inner_meeting_time.include?(@best_condensed_end_time - 1)
+  end
+
+  def inner_start_time
+    @inner_start_time = inner_meeting_time[0].to_i
+  end
+
+  def inner_end_time
+    @inner_end_time = inner_meeting_time[1].to_i
+  end
 end
 
-def start_time
-  @start_time
-end
 
-def end_time
-  @end_time
-end
-
-
-condense_meeting_times([ [0, 1], [3, 5], [4, 8], [10, 12], [9, 10] ])
+FreeTime.new([ [0, 1], [4, 8], [3, 5], [10, 12], [9, 10] ]).condensed_meeting_schedule
 # [ [0, 1], [3, 8], [9, 12] ]
+#[0,12]
